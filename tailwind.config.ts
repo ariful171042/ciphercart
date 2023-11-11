584548b052be2a82a098aeef057e7839300a3cb8
ariful171042 <arif171042@gmail.com>
2023-11-11 12:44:04 +0600

    CipherCart start

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container:{
+      center:true,
+      padding:{
+        default:"1rem",
+        sm:"0"
+      }
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        teal: "#38CABC",
+        black: "#3A3333",
+        gray: "#F4F2F3",
+        white: "#FFFFFF",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
