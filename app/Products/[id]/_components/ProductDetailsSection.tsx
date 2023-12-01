d257fe402224b80d6d35f23118afdb5988adefac
ariful171042 <arif171042@gmail.com>
2023-12-01 23:10:00 +0600

    product details page 50%

--- app/Products/[id]/_components/ProductDetailsSection.tsx
+++ app/Products/[id]/_components/ProductDetailsSection.tsx
@@ -1,3 +1,4 @@
+import Star from '@/components/ui/Star';
 import { productType } from '@/types/productType';
 
 interface ProductDetailsSectionProps {
@@ -7,7 +8,18 @@ interface ProductDetailsSectionProps {
 const ProductDetailsSection: React.FC<ProductDetailsSectionProps> = ({
   product,
 }) => {
-  return <div>ProductDetailsSection</div>;
+  return (
+    <div>
+      <div className='p-5'>
+        <h3>{product.title}</h3>
+        <Star stars={product.star} />
+        <p>{product.desc}</p>
+        <span>{product.category}</span>
+        <p>{product.price}</p>
+        <p>{product.qty}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetailsSection;
