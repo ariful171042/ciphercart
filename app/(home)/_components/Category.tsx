4029431e8ba5a90fe99810170705bbb59915eb5e
ariful171042 <arif171042@gmail.com>
2023-12-02 23:22:06 +0600

    category section

---
+++ app/(home)/_components/Category.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryPage = () => {
+  return (
+    <section>
+      <div className='sp'></div>
+    </section>
+  );
+};
+
+export default CategoryPage;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './_components/Hero';
 import Products from '../products/_components/Products';
+import CategoryPage from './_components/Category';
 
 const HomePage = () => {
   return (
     <main>
       <Hero />
+      <CategoryPage />
       <Products />
     </main>
   );
